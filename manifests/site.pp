89d413d63bc26821490ab1ce029ba403c7c920c1
Neha Thakur <60854924+neha12191@users.noreply.github.com>
2021-02-14 11:36:56 +0000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -24,8 +24,8 @@ File { backup => false }
 # Puppet Enterprise console and External Node Classifiers (ENC's).
 #
 # For more on node definitions, see: https://puppet.com/docs/puppet/latest/lang_node_definitions.html
-node default {
+#node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-}
+#}
