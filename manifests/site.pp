a7d883147aa95bedc36c4bb417bb2fc8954a5dbb
Neha Thakur <60854924+neha12191@users.noreply.github.com>
2021-02-15 17:54:36 +0000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -32,12 +32,6 @@ File { backup => false }
 class {'java' :
    distribution => 'jre',
 }
-java::download { 'jdk8' :
-  ensure  => 'present',
-  java_se => 'jdk',
-  url     => 'http://myjava.repository/java.tgz",
-  basedir => '/custom/java',
-}
 tomcat::install { '/opt/tomcat':
  source_url => 'https://www-us.apache.org/dist/tomcat/tomcat-7/v7.0.x/bin/apache-tomcat-7.0.x.tar.gz',
 }
