05a080ec871328929f1f4e4513c29a47a4b69936
Neha Thakur <60854924+neha12191@users.noreply.github.com>
2021-02-17 18:57:48 +0000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -27,8 +27,8 @@ File { backup => false }
 node default {
   # This is where you can declare classes for all nodes.
   # Example:
-     class { 'OnlineShopping': }
-  include OnlineShopping
+     #class { 'OnlineShopping': }
+  #include OnlineShopping
   #include apt
   #include jenkins
  
