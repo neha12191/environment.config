8666f89fa847a9eca2273c8957e49926af89ba37
Neha Thakur <60854924+neha12191@users.noreply.github.com>
2021-02-18 12:17:09 +0000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -31,6 +31,11 @@ node default {
   #include OnlineShopping
   include tomcat
  # include java::install
-  
+  tomcat::install { '/opt/tomcat':
+  source_url => 'https://www-us.apache.org/dist/tomcat/tomcat-7/v7.0.x/bin/apache-tomcat-7.0.x.tar.gz',
+}
+tomcat::instance { 'default':
+  catalina_home => '/opt/tomcat',
+}
 }
 
