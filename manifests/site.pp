6cf0c21fa2441f844f7100301a56b5401b7a6b13
Neha Thakur <60854924+neha12191@users.noreply.github.com>
2021-04-06 14:24:10 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,20 +5,20 @@ Exec {
 File { backup => false }
 
 node default {
-  include jenkins
+ # include jenkins
 
 class { 'java':
    distribution => 'jre',
  }
 class { 'tomcat': }
-tomcat::install { '/opt/tomcat9':
+tomcat::install { '/opt/tomcat':
   #source_url => 'https://www.mirrorservice.org/sites/ftp.apache.org/tomcat/tomcat-8/v8.5.63/src/apache-tomcat-8.5.63-src.tar.gz',
-  #source_url => 'https://www.mirrorservice.org/sites/ftp.apache.org/tomcat/tomcat-8/v8.5.63/bin/apache-tomcat-8.5.63.tar.gz',
+  source_url => 'https://www.mirrorservice.org/sites/ftp.apache.org/tomcat/tomcat-8/v8.5.63/bin/apache-tomcat-8.5.63.tar.gz',
   #source_url => 'https://www-us.apache.org/dist/tomcat/tomcat-8/v8.5.63/bin/apache-tomcat-8.5.63.tar.gz',
-  source_url => 'https://www.apache.org/dist/tomcat/tomcat-9/v9.0.x/bin/apache-tomcat-9.0.x.tar.gz'
+  #source_url => 'https://www.apache.org/dist/tomcat/tomcat-9/v9.0.x/bin/apache-tomcat-9.0.x.tar.gz'
 }
 tomcat::instance { 'default':
-  catalina_home => '/opt/tomcat9',
+  catalina_home => '/opt/tomcat',
 }
 }
 
