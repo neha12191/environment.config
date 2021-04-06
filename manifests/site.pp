87cf977bf94ff024efd4c9e6feae40b6cac1b4ff
Neha Thakur <60854924+neha12191@users.noreply.github.com>
2021-04-06 14:22:43 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -11,13 +11,14 @@ class { 'java':
    distribution => 'jre',
  }
 class { 'tomcat': }
-tomcat::install { '/opt/tomcat':
+tomcat::install { '/opt/tomcat9':
   #source_url => 'https://www.mirrorservice.org/sites/ftp.apache.org/tomcat/tomcat-8/v8.5.63/src/apache-tomcat-8.5.63-src.tar.gz',
-  source_url => 'https://www.mirrorservice.org/sites/ftp.apache.org/tomcat/tomcat-8/v8.5.63/bin/apache-tomcat-8.5.63.tar.gz',
+  #source_url => 'https://www.mirrorservice.org/sites/ftp.apache.org/tomcat/tomcat-8/v8.5.63/bin/apache-tomcat-8.5.63.tar.gz',
   #source_url => 'https://www-us.apache.org/dist/tomcat/tomcat-8/v8.5.63/bin/apache-tomcat-8.5.63.tar.gz',
+  source_url => 'https://www.apache.org/dist/tomcat/tomcat-9/v9.0.x/bin/apache-tomcat-9.0.x.tar.gz'
 }
 tomcat::instance { 'default':
-  catalina_home => '/opt/tomcat',
+  catalina_home => '/opt/tomcat9',
 }
 }
 
