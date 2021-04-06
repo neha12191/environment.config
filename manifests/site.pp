2a7041293501c67b99e9d0d78f905ce33ce7efa3
Neha Thakur <60854924+neha12191@users.noreply.github.com>
2021-04-06 15:08:27 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,7 @@
 ## site.pp ##
-#Exec {
- #   path => ['/bin', '/usr/bin'],
-#}
+Exec {
+    path => ['/bin', '/usr/bin'],
+}
 File { backup => false }
 
 node default {
@@ -13,11 +13,11 @@ class { 'java':
 class { 'tomcat': }
 tomcat::install { '/opt/tomcat':
   #source_url => 'https://www.mirrorservice.org/sites/ftp.apache.org/tomcat/tomcat-8/v8.5.63/src/apache-tomcat-8.5.63-src.tar.gz',
-  #source_url => 'https://www.mirrorservice.org/sites/ftp.apache.org/tomcat/tomcat-8/v8.5.63/bin/apache-tomcat-8.5.63.tar.gz',
+  source_url => 'https://www.mirrorservice.org/sites/ftp.apache.org/tomcat/tomcat-8/v8.5.63/bin/apache-tomcat-8.5.63.tar.gz',
   #source_url => 'https://www-us.apache.org/dist/tomcat/tomcat-8/v8.5.63/bin/apache-tomcat-8.5.63.tar.gz',
   #source_url => 'https://www.apache.org/dist/tomcat/tomcat-9/v9.0.x/bin/apache-tomcat-9.0.x.tar.gz'
   #source_url => 'http://mirror.nexcess.net/apache/tomcat/tomcat-8/v8.0.8/bin/apache-tomcat-8.0.8.tar.gz'
-  source_url => 'https://www-us.apache.org/dist/tomcat/tomcat-8/v8.5.28/bin/apache-tomcat-8.5.28.tar.gz',
+  #source_url => 'https://www-us.apache.org/dist/tomcat/tomcat-8/v8.5.28/bin/apache-tomcat-8.5.28.tar.gz',
 }
 tomcat::instance { 'default':
   catalina_home => '/opt/tomcat',
