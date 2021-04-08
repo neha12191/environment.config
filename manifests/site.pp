9287a98527b1f431cde2219988a89ac10f5c4081
Neha Thakur <60854924+neha12191@users.noreply.github.com>
2021-04-08 14:58:15 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,9 +7,9 @@ File { backup => false }
 node default {
   include jenkins
 
-class { 'java':
-   distribution => 'jre',
- }
+#class { 'java':
+   #distribution => 'jre',
+ #}
 class { 'tomcat': }
 tomcat::install { '/opt/tomcat':
   #source_url => 'https://www.mirrorservice.org/sites/ftp.apache.org/tomcat/tomcat-8/v8.5.63/src/apache-tomcat-8.5.63-src.tar.gz',
