2ee8e13a7da93f0c1afa6a7aad74c1f506f817cb
Neha Thakur <60854924+neha12191@users.noreply.github.com>
2021-05-23 00:47:18 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -13,11 +13,8 @@ class { 'java':
 class { 'tomcat': }
 tomcat::install { '/opt/tomcat':
   #source_url => 'https://www.mirrorservice.org/sites/ftp.apache.org/tomcat/tomcat-8/v8.5.63/src/apache-tomcat-8.5.63-src.tar.gz',
-  source_url => 'https://www.mirrorservice.org/sites/ftp.apache.org/tomcat/tomcat-8/v8.5.65/bin/apache-tomcat-8.5.65.tar.gz',
-  #source_url => 'https://www-us.apache.org/dist/tomcat/tomcat-8/v8.5.63/bin/apache-tomcat-8.5.63.tar.gz',
-  #source_url => 'https://www.apache.org/dist/tomcat/tomcat-9/v9.0.x/bin/apache-tomcat-9.0.x.tar.gz'
-  #source_url => 'http://mirror.nexcess.net/apache/tomcat/tomcat-8/v8.0.8/bin/apache-tomcat-8.0.8.tar.gz'
-  #source_url => 'https://www-us.apache.org/dist/tomcat/tomcat-8/v8.5.28/bin/apache-tomcat-8.5.28.tar.gz',
+  source_url => 'https://www.mirrorservice.org/sites/ftp.apache.org/tomcat/tomcat-8/v8.5.66/bin/apache-tomcat-8.5.66.tar.gz',
+
 }
 tomcat::instance { 'default':
   catalina_home => '/opt/tomcat',
